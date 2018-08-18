a180949f0138d399cc80cf3a2772a4d07cde3d2e
Rob Cunning <rjacunning@gmail.com>
2018-08-19 00:46:19 +1000

    - Setup table dimensions - Setup directions - Implemented robot commands - Bot can now report to user, will complain if placed off the table or given a command it doesn't understand - Added position validation - Gave the robot some personality

--- app/robot/index.js
+++ app/robot/index.js
@@ -1,18 +1,142 @@
 let colors = require('colors/safe');
 
+let TABLE_DIMENSIONS = [5, 5];
+/*
+Grid reference of the table
+|---|---|---|---|---|
+|0,4|1,4|2,4|3,4|4,4|
+|---|---|---|---|---|
+|0,3|1,3|2,3|3,3|4,3|
+|---|---|---|---|---|
+|0,2|1,2|2,2|3,2|4,2|
+|---|---|---|---|---|
+|0,1|1,1|2,1|3,1|4,1|
+|---|---|---|---|---|
+|0,0|1,0|2,0|3,0|4,0|
+|---|---|---|---|---|
+*/
+
+let DIRECTIONS = {
+  NORTH: { x: 0, y: 1, left: 'WEST', right: 'EAST' },
+  EAST: { x: 1, y: 0, left: 'NORTH', right: 'SOUTH' },
+  SOUTH: { x: 0, y: -1, left: 'EAST', right: 'WEST' },
+  WEST: { x: -1, y: 0, left: 'SOUTH', right: 'NORTH' }
+};
+
 function Robot() {
   const self = this;
+  self.posX = null;
+  self.posY = null;
+  self.facing = null;
 
+  /**
+   * Execute a robot command
+   * @param {string} action - Name of the command
+   * @param {array} args - An array of arguments
+   */
+  self.command = (action, args) => {
+    const command = self[action.toLowerCase()];
+    if (!command) {
+      self._error(
+        `I don\'t understand the command "${action}", type "help" for a list of commands I understand`
+      );
+      return;
+    }
+
+    command.apply(self, args);
+  };
+
+  /**
+   * Place the robot on the grid
+   * @param {Number} posX - The X position of the robot
+   * @param {Number} posY - The Y position of the robot
+   * @param {string} facing - The direction the robot is facing
+   * possible values are NORTH, SOUTH, EAST, WEST
+   * @return {Boolean} - Returns true if the robot was placed
+   */
+  self.place = (posX, posY, facing) => {
+    if (!self._validatePosition(posX, posY)) {
+      self._error(
+        'You cannot place me off the table, type "help" to see the tables dimensions'
+      );
+      return false;
+    }
+
+    if (typeof DIRECTIONS[facing] == 'undefined') {
+      self._error(
+        'That is an invalid direction, type "help" to see which directions I can face.'
+      );
+      return false;
+    }
+
+    self.posX = parseInt(posX, 10);
+    self.posY = parseInt(posY, 10);
+    self.facing = facing;
+    self.report();
+
+    return true;
+  };
+
+  // Report the current position of the robot
+  self.report = () => {
+    if (!self._hasPlaced()) {
+      return;
+    }
+
+    self._info(
+      'I am now at position (' +
+        self.posX +
+        ', ' +
+        self.posY +
+        ') and I am facing ' +
+        self.facing
+    );
+  };
+
+  // Robot info handler
   self._info = message => {
-    self._speak('green', message);
+    self._speak('green', '', message);
   };
 
+  // Robot error handler
   self._error = message => {
-    self._speak('red', message);
+    self._speak('red', 'The robot looks at you quizically', message);
   };
 
-  self._speak = (color, message) => {
-    console.log(colors[color].underline('ROBOT SAYS: ' + message));
+  // Helper function that lets the robot speak to the user
+  self._speak = (color, prefix, message) => {
+    if (prefix) {
+      console.log(colors.magenta(prefix));
+    }
+    console.log(
+      colors[color].underline('TOY ROBOT SAYS: Beep boop - ' + message)
+    );
+  };
+
+  /**
+   * Validate a future position of the robot
+   * @param {Number} x - The future x value
+   * @param {Number} y - The future y value
+   * @return {Boolean} - Returns true if the Robot can move to that position
+   */
+  self._validatePosition = (x, y) => {
+    if (x < 0 || x > TABLE_DIMENSIONS[0] - 1) {
+      return false;
+    }
+
+    if (y < 0 || y > TABLE_DIMENSIONS[1] - 1) {
+      return false;
+    }
+
+    return true;
+  };
+
+  /**
+   * Check if the robot has been placed
+   * @return {Boolean} - Returns true if the robot is placed on the grid
+   */
+  self._hasPlaced = () => {
+    return self.posX !== null && self.posY !== null && self.facing !== null;
   };
 
   return self;
--- index.js
+++ index.js
@@ -5,6 +5,19 @@ let inquirer = require('inquirer');
 let Robot = require('./app/robot');
 var robot = new Robot();
 
+const tableString =
+  '|---|---|---|---|---|\n\
+|0,4|1,4|2,4|3,4|4,4|\n\
+|---|---|---|---|---|\n\
+|0,3|1,3|2,3|3,3|4,3|\n\
+|---|---|---|---|---|\n\
+|0,2|1,2|2,2|3,2|4,2|\n\
+|---|---|---|---|---|\n\
+|0,1|1,1|2,1|3,1|4,1|\n\
+|---|---|---|---|---|\n\
+|0,0|1,0|2,0|3,0|4,0|\n\
+|---|---|---|---|---|';
+
 console.log(colors.rainbow('*'.repeat(50)));
 console.log(colors.rainbow(figlet.textSync('toyrobotjs', {})));
 console.log(colors.rainbow('*'.repeat(50)));
@@ -25,12 +38,12 @@ const help = () => {
     )
   );
   console.log(colors.yellow('PLACE X,Y,F (facing direction) - for example:'));
-  console.log(colors.cyan('PLACE 0,0,NORTH'));
-  console.log(colors.magenta('*'.repeat(50)));
+  console.log(colors.cyan('PLACE 0,3,NORTH'));
   console.log(
     colors.magenta('The robot can only face NORTH, SOUTH, EAST or WEST')
   );
-  console.log(colors.magenta('*'.repeat(50)));
+  console.log(colors.magenta('The table is a 5 x 5 grid'));
+  console.log(colors.magenta(tableString));
   console.log(
     colors.yellow(
       '\nThen use the following commands to interact with the robot:'
@@ -62,6 +75,7 @@ const help = () => {
   );
 };
 
+// Using inquirer to ask the user questions and handle users answers
 function prompt() {
   inquirer.prompt(questions).then(answers => {
     if (answers.command.toLowerCase() === 'help') {
